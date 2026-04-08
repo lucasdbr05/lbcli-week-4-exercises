481bb71cce35fabf6a7effd4d7ae014534b908a3
lucasdbr05 <lucasgabrieljake@gmail.com>
2026-04-08 16:32:17 -0300

    solve 03

--- submission/03.sh
+++ submission/03.sh
@@ -2,4 +2,30 @@
 
 # Amount of 20,000,000 satoshis to this address: 2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP 
 # Use the UTXOs from the transaction below
-# transaction="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+transaction="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+address="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
+
+txid=$(bitcoin-cli -regtest decoderawtransaction $transaction | jq -r '.txid')
+
+inputs=$(bitcoin-cli -regtest decoderawtransaction $transaction | jq -c \
+'[.vout | to_entries[] | {
+  txid: "'$txid'",
+  vout: .key,
+  sequence: 0
+}]')
+
+
+msg_hex=$(echo -n "btrust builder 2026" | xxd -p -c 200)
+
+outputs=$(jq -n \
+--arg addr "2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP" \
+--arg data "$msg_hex" \
+'{
+  ($addr): 0.2,
+  "data": $data
+}')
+
+
+rawtx=$(bitcoin-cli -regtest createrawtransaction "$inputs" "$outputs")
+
+echo $rawtx
